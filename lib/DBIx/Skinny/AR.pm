64d5e303718f3f68c71af94a0290e2c8c3683953
Ryo Miyake <ryo.studiom@gmail.com>
2009-09-07 10:42:57 +0900

    wrote pod

--- lib/DBIx/Skinny/AR.pm
+++ lib/DBIx/Skinny/AR.pm
@@ -263,119 +263,86 @@ sub _get_suffix {
     return $class;
 }
 
-1; # Magic true value required at end of module
+1;
 __END__
 
 =head1 NAME
 
-DBIx::Skinny::AR - [One line description of module's purpose here]
-
-
-=head1 VERSION
-
-This document describes DBIx::Skinny::AR version 0.0.1
+DBIx::Skinny::AR - DBIx::Skinny's wrapper like ActiveRecord
 
 
 =head1 SYNOPSIS
 
-    use DBIx::Skinny::AR;
+setup your ar model (DBIx::Skinny already setup as MyApp::DB and MyApp::DB::Schema)
 
-=for author to fill in:
-    Brief code example(s) here showing commonest usage(s).
-    This section will be as far as many users bother reading
-    so make it as educational and exeplary as possible.
-  
-  
+    package MyApp::DB::AR;
+    use Any::Moose;
+    extends 'DBIx::Skinny::AR';
+
+    __PACKAGE__->setup({
+        db => 'MyApp::DB',
+        validator => {
+            module  => 'FormValidator::Simple',
+            plugins => [qw(
+                FormValidator::Simple::Plugin::DBIC::Unique
+                FormValidator::Simple::Plugin::Japanese
+                FormValidator::Simple::Plugin::NetAddr::IP
+            )],
+        },
+    });
+
+    1;
+
+create each model class
+
+    package MyApp::Book;
+    use Any::Moose;
+    extends 'MyApp::DB::AR';
+
+    __PACKAGE__->mk_accessors;
+
+    sub table { 'books' }
+
+    sub default_search_column { 'name' }
+
+    sub validation {
+        [
+            id           => [ qw/UINT/ ],
+            author_id    => [ qw/NOT_BLANK UINT/ ],
+            name         => [ qw/NOT_BLANK ASCII/, [ qw/LENGTH 0 255/ ] ],
+            { name => [ qw/id name/ ] } => [ [ 'DBIC_UNIQUE', __PACKAGE__, '!id', 'name' ] ],
+        ];
+    }
+
+    __PACKAGE__->belongs_to('author');
+
+    1;
+
+in your apps
+
+    use MyApp::Book;
+    my $book = MyApp::Book->find($book_name);
+    $book->name('new name');
+    $book->update;
+
+    my $books = MyApp::Book->find_all;
+
+    my $author = $book->author;
+
+
 =head1 DESCRIPTION
 
-=for author to fill in:
-    Write a full description of the module and its features here.
-    Use subsections (=head2, =head3) as appropriate.
+DBIx::Skinny::AR provides some interfaces like ActiveRecord.
 
-
-=head1 INTERFACE 
-
-=for author to fill in:
-    Write a separate section listing the public components of the modules
-    interface. These normally consist of either subroutines that may be
-    exported, or methods that may be called on objects belonging to the
-    classes provided by the module.
-
-
-=head1 DIAGNOSTICS
-
-=for author to fill in:
-    List every single error and warning message that the module can
-    generate (even the ones that will "never happen"), with a full
-    explanation of each problem, one or more likely causes, and any
-    suggested remedies.
-
-=over
-
-=item C<< Error message here, perhaps with %s placeholders >>
-
-[Description of error here]
-
-=item C<< Another error message here >>
-
-[Description of error here]
-
-[Et cetera, et cetera]
-
-=back
-
-
-=head1 CONFIGURATION AND ENVIRONMENT
-
-=for author to fill in:
-    A full explanation of any configuration system(s) used by the
-    module, including the names and locations of any configuration
-    files, and the meaning of any environment variables or properties
-    that can be set. These descriptions must also include details of any
-    configuration language used.
-  
-DBIx::Skinny::AR requires no configuration files or environment variables.
-
-
-=head1 DEPENDENCIES
-
-=for author to fill in:
-    A list of all the other modules that this module relies upon,
-    including any restrictions on versions, and an indication whether
-    the module is part of the standard Perl distribution, part of the
-    module's distribution, or must be installed separately. ]
-
-None.
-
-
-=head1 INCOMPATIBILITIES
-
-=for author to fill in:
-    A list of any modules that this module cannot be used in conjunction
-    with. This may be due to name conflicts in the interface, or
-    competition for system or program resources, or due to internal
-    limitations of Perl (for example, many modules that use source code
-    filters are mutually incompatible).
-
-None reported.
+ - find/find_all by any conditions
+ - return object wrapped DBIx::Skinny::Row
+ - validate before create/update
+ - support belongs_to/has_one/has_many/many_to_many relationships
 
 
 =head1 BUGS AND LIMITATIONS
 
-=for author to fill in:
-    A list of known problems with the module, together with some
-    indication Whether they are likely to be fixed in an upcoming
-    release. Also a list of restrictions on the features the module
-    does provide: data types that cannot be handled, performance issues
-    and the circumstances in which they may arise, practical
-    limitations on the size of data sets, special cases that are not
-    (yet) handled, etc.
-
 No bugs have been reported.
-
-Please report any bugs or feature requests to
-C<bug-dbix-skinny-ar@rt.cpan.org>, or through the web interface at
-L<http://rt.cpan.org>.
 
 
 =head1 AUTHOR
@@ -391,25 +358,6 @@ This module is free software; you can redistribute it and/or
 modify it under the same terms as Perl itself. See L<perlartistic>.
 
 
-=head1 DISCLAIMER OF WARRANTY
+=head1 SEE ALSO
 
-BECAUSE THIS SOFTWARE IS LICENSED FREE OF CHARGE, THERE IS NO WARRANTY
-FOR THE SOFTWARE, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN
-OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES
-PROVIDE THE SOFTWARE "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER
-EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE
-ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE SOFTWARE IS WITH
-YOU. SHOULD THE SOFTWARE PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL
-NECESSARY SERVICING, REPAIR, OR CORRECTION.
-
-IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING
-WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MAY MODIFY AND/OR
-REDISTRIBUTE THE SOFTWARE AS PERMITTED BY THE ABOVE LICENCE, BE
-LIABLE TO YOU FOR DAMAGES, INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL,
-OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR INABILITY TO USE
-THE SOFTWARE (INCLUDING BUT NOT LIMITED TO LOSS OF DATA OR DATA BEING
-RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD PARTIES OR A
-FAILURE OF THE SOFTWARE TO OPERATE WITH ANY OTHER SOFTWARE), EVEN IF
-SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF
-SUCH DAMAGES.
+DBIx::Skinny
