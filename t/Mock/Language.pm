b12220af5b13eb6d9d7528ae5297c5ab4a566063
Ryo Miyake <ryo.studiom@gmail.com>
2009-09-23 19:36:35 +0900

    added unique constraint

--- t/Mock/Language.pm
+++ t/Mock/Language.pm
@@ -4,8 +4,16 @@ extends 'Mock::AR';
 
 use Carp;
 
-has 'id'   => (is => 'rw', isa => 'Undef | Int');
-has 'name' => (is => 'rw', isa => 'Str');
+has 'id' => (
+    is  => 'rw',
+    isa => 'Undef | Int',
+);
+
+has 'name' => (
+    is      => 'rw',
+    isa     => 'Str',
+    trigger => sub { shift->chk_unique('name') },
+);
 
 #__PACKAGE__->many_to_many('members' => { glue => 'member_languages' });
 
