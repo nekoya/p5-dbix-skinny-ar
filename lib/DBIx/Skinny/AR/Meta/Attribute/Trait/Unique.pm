2f3315c7cc7f9f6624346bec5e469c681b5d63f6
Ryo Miyake <ryo.studiom@gmail.com>
2009-10-04 00:10:07 +0900

    Divided Trait class

---
+++ lib/DBIx/Skinny/AR/Meta/Attribute/Trait/Unique.pm
@@ -0,0 +1,26 @@
+package DBIx::Skinny::AR::Meta::Attribute::Trait::Unique;
+use Any::Moose '::Role';
+
+after 'install_accessors' => sub {
+    my $self = shift;
+    my $meta = $self->associated_class;
+    for my $attr ( $meta->get_all_attributes ) {
+        if ( $attr->does('DBIx::Skinny::AR::Meta::Attribute::Trait::Unique') ) {
+            my $name = $attr->name;
+            $meta->add_after_method_modifier(
+                $name,
+                sub {
+                    my $self = shift;
+                    $self->_chk_unique_value($name) if @_;
+                }
+            );
+        }
+    }
+};
+
+local $@;
+eval "package " . any_moose . "::Meta::Attribute::Custom::Trait::Unique;\n".
+     "sub register_implementation {'DBIx::Skinny::AR::Meta::Attribute::Trait::Unique'}";
+die $@ if $@;
+
+1;
